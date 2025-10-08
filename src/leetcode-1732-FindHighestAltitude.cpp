70cb8106f3c333a8facfe9c6ad76823c2b2fa4fe
RoboCrafty <fuzailhamid2002@gmail.com>
2025-10-08 21:38:15 +0200

    Push LeetCode-1732

--- src/leetcode-1732-FindHighestAltitude.cpp
+++ src/leetcode-1732-FindHighestAltitude.cpp
@@ -1,6 +1,29 @@
+#include <iostream>
+
 class Solution{
 public: 
-        int largestAltitude(vector)
+        int largestAltitude(std::vector<int>& gain)
+        {
+                int size1 = gain.size();
+                int maxAlt = 0;
+                int currentAlt = 0;
 
+                for(int i = 0; i < size1; i++)
+                {
+                        currentAlt += gain[i];
+                        
+                        if (currentAlt > maxAlt)
+                        {
+                                maxAlt = currentAlt;
+                        }
+                }
+                return maxAlt;
+        }
+};
 
+int main()
+{
+        Solution sol;
+        std::vector<int> gain = {28,0,-8,-99,11,62,-35,68,2,12,-71,13,66,-28};
+        std::cout << "Max Altitute is: " << sol.largestAltitude(gain) << std::endl;
 }
